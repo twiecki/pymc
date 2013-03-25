3d61caafca9a0406bb0bf10b1a3c8f3f1d48d2c7
Thomas Wiecki <thomas.wiecki@gmail.com>
2013-03-25 13:59:14 -0700

    BLD: Added patsy install.

--- pymc/distributions/continuous.py
+++ pymc/distributions/continuous.py
@@ -6,7 +6,7 @@ nodes in PyMC.
 
 """
 
-from dist_math import * 
+from dist_math import *
 
 __all__ = ['Uniform', 'Flat', 'Normal', 'Beta','Exponential', 'T', 'Cauchy', 'Gamma', 'Bound', 'Tpos']
 
@@ -37,36 +37,36 @@ def Uniform(lower=0, upper=1):
 
     default = mean
 
-    
+
     logp.__doc__ = """
         Uniform log-likelihood with parameters lower={0} and upper={1}.
-        
+
         Parameters
         ----------
         value : float
             :math:`lower \leq x \leq upper`
-        """              
-    
+        """
+
     return locals()
 
 @quickclass
 def Flat():
     """
-    Uninformative log-likelihood that returns 0 regardless of 
+    Uninformative log-likelihood that returns 0 regardless of
     the passed value.
     """
     support = 'continuous'
-    
+
     def logp(value):
         return zeros_like(value)
-        
+
     logp.__doc__ = """
-        Uninformative log-likelihood that returns 0 regardless of 
+        Uninformative log-likelihood that returns 0 regardless of
         the passed value.
         """
 
     median = 0
-        
+
     return locals()
 
 @quickclass
@@ -93,9 +93,9 @@ def Normal(mu=0.0, tau=1.0):
     support = 'continuous'
 
     def logp(value):
-        
+
         return bound(
-                (-tau * (value-mu)**2 + log(tau/pi/2.))/2., 
+                (-tau * (value-mu)**2 + log(tau/pi/2.))/2.,
                 tau > 0)
 
     mean = mu
@@ -103,16 +103,16 @@ def Normal(mu=0.0, tau=1.0):
 
     median = mean
     mode = mean
-    
+
     logp.__doc__ = """
         Normal log-likelihood with paraemters mu={0} and tau={1}.
-        
+
         Parameters
         ----------
         value : float
             Input data.
         """.format(mu, tau)
-    
+
     return locals()
 
 @quickclass
@@ -138,7 +138,7 @@ def Beta(alpha, beta):
     """
     support = 'continuous'
     def logp(value):
-        
+
         return bound(
                   gammaln(alpha+beta) - gammaln(alpha) - gammaln(beta) + logpow(value, alpha -1) + logpow(1-value, beta-1),
                   0 <= value, value <= 1,
@@ -148,29 +148,29 @@ def Beta(alpha, beta):
     mean = alpha/(alpha + beta)
     variance = alpha*beta/(
             (alpha + beta)**2 * (alpha + beta +1))
-                  
+
     logp.__doc__ = """
     Beta log-likelihood with parameters alpha={0} and beta={1}.
-        
+
     Parameters
     ----------
     value : float
         0 < x < 1
     """.format(alpha, beta)
-                  
+
     return locals()
 
 @quickclass
 def Exponential(lam):
     """
     Exponential distribution
-    
+
     Parameters
     ----------
-    lam : float 
-        lam > 0 
+    lam : float
+        lam > 0
         rate or inverse scale
-    """ 
+    """
 
     support = 'continuous'
     mean = 1./lam
@@ -191,7 +191,7 @@ def T(nu, mu=0, lam=1):
     Non-central Student's T log-likelihood.
 
     Describes a normal variable whose precision is gamma distributed. If
-    only nu parameter is passed, this specifies a standard (central) 
+    only nu parameter is passed, this specifies a standard (central)
     Student's T.
 
     .. math::
@@ -210,11 +210,11 @@ def T(nu, mu=0, lam=1):
         Scale parameter (defaults to 1)
     """
     support = 'continuous'
-    
+
     def logp(value):
         return bound(
                   gammaln((nu+1.0)/2.0) + .5 * log(lam / (nu * pi)) - gammaln(nu/2.0) - (nu+1.0)/2.0 * log(1.0 + lam *(value - mu)**2/nu),
-                  lam > 0, 
+                  lam > 0,
                   nu > 0)
 
     mean = mu
@@ -223,15 +223,15 @@ def T(nu, mu=0, lam=1):
 
     logp.__doc__ = """
         Student's T log-likelihood with paramters nu={0}, mu={1} and lam={2}.
-        
+
         Parameters
         ----------
         value : float
             Input data
         """
-    
+
     return locals()
-    
+
 @quickclass
 def Cauchy(alpha, beta):
     """
@@ -252,22 +252,22 @@ def Cauchy(alpha, beta):
     Mode and median are at alpha.
 
     """
-    support = 'continuous' 
+    support = 'continuous'
     def logp(value):
         return bound(
                   -log(pi) -log(beta) - log( 1 + ((value-alpha) / beta) ** 2 ),
                   beta > 0)
-                  
+
     logp.__doc__ = """
         Cauchy log-likelihood with parameters alpha={0} and beta={0}.
-        
+
         Parameters
         ----------
         value : float
             Input data.
         """.format(alpha, beta)
     return locals()
-    
+
 @quickclass
 def Gamma(alpha, beta):
     """
@@ -293,7 +293,7 @@ def Gamma(alpha, beta):
     - :math:`Var(X) = \frac{\alpha}{\beta^2}`
 
     """
-    support = 'continuous' 
+    support = 'continuous'
 
     mean = alpha/beta
     median = max((alpha -1)/beta, 0)
@@ -303,19 +303,19 @@ def Gamma(alpha, beta):
         return bound(
                 -gammaln(alpha) + logpow(beta, alpha) - beta*value + logpow(value, alpha-1),
 
-                value >= 0, 
-                alpha > 0, 
+                value >= 0,
+                alpha > 0,
                 beta > 0)
 
     logp.__doc__ = """
         Gamma log-likelihood with paramters alpha={0} and beta={1}.
-        
+
         Parameters
         ----------
         x : float
             math:`x \ge 0`
         """.format(alpha, beta)
-    
+
     return locals()
 
 
@@ -325,8 +325,46 @@ def Bound(dist, lower = -inf, upper = inf):
     def logp(value):
         return bound(
                 dist.logp(value),
-                
+
                 lower <= value, value <= upper)
+
+    return locals()
+
+@quickclass
+def Wfpt(a, v, z):
+    support = 'continuous'
+
+    def logp(value):
+        p, k = T.dscalars(['k', 'p'])
+        tt = value/a**2
+        kl = T.switch(np.pi*tt*err<1, # if
+                      T.max(T.stack(T.sqrt(-2*T.log(np.pi*tt*err)/(np.pi**2*tt)), 1/(np.pi*T.sqrt(tt))), axis=0), # then
+                      1/(np.pi*T.sqrt(tt))) # else
+
+        ks = T.switch(2*T.sqrt(2*np.pi*tt)*err<1, # if error threshold is set low enough
+                      T.max(T.stack(2+T.sqrt(-2*tt*T.log(2*T.sqrt(2*np.pi*tt)*err)), # bound
+                                    T.sqrt(tt)+1), axis=0), # ensure boundary conditions are met
+                      2) # if error threshold was set too high -> minimal kappa for that case
+        Ks = T.ceil(ks)
+        Kl = T.ceil(kl)
+
+        ps = lambda k, z, tt: T.sum((z+2*k)*T.exp(-((z+2*k)**2)/2/tt))
+        pl = lambda k, z, tt: T.sum(k*T.exp(-(k**2)*(np.pi**2)*tt/2)*T.sin(k*np.pi*z))
+
+        ps_range = T.arange(-T.floor((Ks-1)/2), T.ceil((Ks-1)/2.), 1)
+        pl_range = T.arange(1, Kl, 1)
+
+        ps_terms, tmp = theano.map(ps, ps_range, non_sequences=(z, tt))
+        pl_terms, tmp = theano.map(pl, pl_range, non_sequences=(z, tt))
+
+        ps_out = ps_terms.sum() * np.pi
+        pl_out = pl_terms.sum() / T.sqrt(2*np.pi*T.pow(tt,3))
+
+        p_out = T.switch(ks < kl, ps_out, pl_out)
+
+        out = p_out*T.exp(-v*a*z -(T.pow(v,2))*value/2.)/(T.pow(a,2))
+
+        return log(out)
 
     return locals()
 
